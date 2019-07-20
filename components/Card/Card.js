83ec207d32183968161f6fc9d98ead90f0637e24
ryanboris <ryan.boris@gmail.com>
2019-07-20 17:54:12 -0400

    :bug: Slow down when you do the server next time

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,9 +1,50 @@
-const url = 'https://my-json-server.typicode.com/ryanboris/instaclone/data'
+const url = 'https://my-json-server.typicode.com/ryanboris/instaclone/db'
+
+const postMaker = post => {
+  // Define all DOM related variable
+  // Grab/SELECT the 'hook'
+  const app = document.querySelector('#app')
+  // CREATE elements
+  const postContainer = document.createElement('div')
+  const username = document.createElement('div')
+  const thumbnailUrl = document.createElement('img')
+  const imageUrl = document.createElement('img')
+  const likes = document.createElement('div')
+  const timestamp = document.createElement('div')
+
+  // Appends
+
+  app.appendChild(postContainer)
+  postContainer.appendChild(username)
+  postContainer.appendChild(thumbnailUrl)
+  postContainer.appendChild(imageUrl)
+  postContainer.appendChild(likes)
+  postContainer.appendChild(timestamp)
+
+  // Classes
+  postContainer.className = 'post'
+  username.className = 'username'
+  thumbnailUrl.className = 'thumbnail'
+  imageUrl.className = 'img-url'
+  likes.className = 'likes'
+  timestamp.className = 'timestamp'
+
+  // TextContent
+  username.textContent = post.username
+  likes.textContent = post.likes
+  timestamp.textContent = post.timestamp
+
+  // Imgs
+  thumbnailUrl.setAttribute('src', post.thumbnailUrl)
+  imageUrl.setAttribute('src', post.imageUrl)
+}
 
 axios
   .get(url)
   .then(response => {
-    console.log(response.data)
+    response.data.posts.map(post => {
+      postMaker(post)
+    })
   })
   .catch(err => {
     console.error(err)
